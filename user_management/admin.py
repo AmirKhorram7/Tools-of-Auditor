364e0d0545091ab5f5282fb05419dbea231921a0
AmirKhorram7 <amireducationkhorramniaki@gmail.com>
2026-08-10 16:21:37 +0330

    add and update something in the django admin panle

--- user_management/admin.py
+++ user_management/admin.py
@@ -16,10 +16,10 @@ class ProfileInline(admin.StackedInline):
 @admin.register(CustomUser)
 class CustomUserAdmin(BaseUserAdmin):
     model = CustomUser
-    list_display = ("phone_number", "is_phone_verified", "is_staff", "is_active")
+    list_display = ("id","first_name","last_name", "phone_number", "is_phone_verified", "is_staff", "is_active")
     list_filter = ("is_phone_verified", "is_staff", "is_active")
     search_fields = ("phone_number", "first_name", "last_name")
-    ordering = ("phone_number",)
+    ordering = ("id",)
     fieldsets = (
         (None, {"fields": ("phone_number", "password")}),
         (_("Personal info"), {"fields": ("first_name", "last_name")}),
@@ -43,7 +43,7 @@ class CustomUserAdmin(BaseUserAdmin):
 
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
-    list_display = ("user", "company_name", "job_title", "created_at")
+    list_display = ("id", "user", "user__first_name", "user__last_name", "company_name", "job_title", "created_at")
     search_fields = ("user__phone_number", "company_name", "job_title")
     readonly_fields = ("created_at", "updated_at")
 
