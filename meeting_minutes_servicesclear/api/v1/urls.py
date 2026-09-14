72ed6ed546ee9de36fb859811cc80a8a462aa8b3
AmirKhorram7 <amireducationkhorramniaki@gmail.com>
2026-09-14 09:50:40 +0330

    add urls for groups and invitation

--- meeting_minutes_servicesclear/api/v1/urls.py
+++ meeting_minutes_servicesclear/api/v1/urls.py
@@ -1,13 +1,25 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from meeting_minutes_servicesclear.api.v1.views import CompanyViewSet
+from meeting_minutes_servicesclear.api.v1.views import (
+    #company views
+    CompanyViewSet,
+
+    #group views
+    GroupViewSet,
+    InvitationViewSet,
+)
+
 
 
 
 
 
 router = DefaultRouter()
-router.register(r"companies", CompanyViewSet, basename="minutes-companies")
+router.register("companies", CompanyViewSet, basename="minutes-companies")
+router.register("groups", GroupViewSet, basename="minutes-groups")
+router.register("invitations", InvitationViewSet, basename="minutes-invitations")
+
+
 
 urlpatterns = [
     path("", include(router.urls)),
